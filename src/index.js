8b25aaa9e22ca5f169e8c6043b99edfb2b934611
sergdomina <80458079+sergdomina@users.noreply.github.com>
2022-01-24 14:58:08 +0200

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,43 @@
 import './css/styles.css';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import { fechCountries } from './js/services/fetch-coutries';
+import countriesList from './js/templeites/countriesMarkup.hbs'
+import oneCountriesMarkup from './js/templeites/onecountryMarkup.hbs'
+import debounce from 'lodash.debounce';
 
 const DEBOUNCE_DELAY = 300;
+
+const searchBox=document.querySelector('#search-box')
+const ulBox=document.querySelector('.country-list')
+const divBox=document.querySelector('.country-info')
+searchBox.addEventListener('input',debounce(handlInput,DEBOUNCE_DELAY))
+function handlInput(event){
+
+    const userInput=event.target.value.trim()
+    if(userInput===''){
+        ulBox.innerHTML=''
+        divBox.innerHTML=''
+        return
+    }
+    fechCountries(userInput).then(showCountries).catch(showError)
+}
+function showCountries(countries){
+    ulBox.innerHTML=''
+    divBox.innerHTML=''
+
+    if(countries.length>10){
+        return Notify.success("Too many matches found. Please enter a more specific name.");
+    }
+    if(countries.length>=2 && countries.length<=10){
+        const markUp=countriesList(countries)
+        ulBox.insertAdjacentHTML('beforeend',markUp)
+        return
+    }
+    const countryMarkup=oneCountriesMarkup(countries)
+    divBox.insertAdjacentHTML('beforeend',countryMarkup)
+}
+function showError(){
+    ulBox.innerHTML=''
+    divBox.innerHTML=''
+    return Notify.failure("Oops, there is no country with that name");
+}
---
+++ src/js/services/fetch-coutries.js
@@ -0,0 +1,9 @@
+export function fechCountries(name){
+    const url= `https://restcountries.com/v2/name/${name}?fields=name,capital,population,flags,languages`
+    return fetch(url).then(respons=>{
+        if(!respons.ok){
+            throw Error(respons.statusText)
+        }
+        return respons.json()
+    })
+}
